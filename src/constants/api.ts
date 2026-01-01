06a17e7bf193cba298707133b29103ebddd34dce
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2026-01-01 16:55:55 +0200

    The interest & behavior is work!

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "https://campaigner-ai-backend.onrender.com"; // Change to "http://127.0.0.1:8000" while running from localhost 
+export const API_BASE_URL = "http://127.0.0.1:8006"; // Use "http://127.0.0.1:8000" while running from localhost and "https://campaigner-ai-backend.onrender.com" when deployed
